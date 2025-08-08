aa7c5fce562c953435bee129aecf37dd4e55e55b
y0puzz <m18751627177@163.com>
2025-08-08 22:35:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
+	title: "y0puz",
 	subtitle: "Demo Site",
 	lang: "en", // Language code, e.g. 'en', 'zh-CN', 'ja', etc.
 	themeColor: {
